ea9ab5c3fef620d54d94ee8163202c1b9302644b
jotanico <129216765+jotanico@users.noreply.github.com>
2023-04-12 21:15:42 -0400

    Add files via upload

---
+++ 01-variables.py
@@ -0,0 +1,28 @@
+print ("hola a todos")
+
+###Nombre  de la variento
+nombre = "Jorge Abarzua"
+
+#Impresion de una variable
+print ("hola a todos, me llamo" , nombre)
+
+#declarando una tercera variable
+edad = "18 años"
+
+#impresion a la segunda  variable
+print ("Y tengo ", edad)
+
+#diabblillo
+#intento print ("hola mi nombre es", + nombre + "y tengo", edad + años)
+print ("hola mi nombre es", str(nombre),"y tengo", str(edad))
+#actu de nombre
+nombre2= "pepito"
+print("yo soy", nombre2)
+#utilizando la entrada de instrucciones
+#a
+nombre2= ("¿cual es tu nombre2? \n")
+print ("Tu nombre es", nombre2)
+
+nombre = input("Dime cual es tu nombre \n")
+edad  = input ("Ahora dime tu edad \n")
+print("entonces tu nombre es", str (nombre), "y tienes", str (edad))
