1baa935101e570cb044329bdaa1f9f4e86897d67
jotanico <129216765+jotanico@users.noreply.github.com>
2023-05-08 16:00:58 -0400

    Add files via upload

---
+++ reto1.py
@@ -0,0 +1,4 @@
+nombre = input(f"¿cual es tu nombre? \n" )
+Edad = input((f"¿Cuantos años tienes? \n"))
+Total_edad = Edad+20
+print("Hola mi nombre es", str(nombre), "y tengo", type(Edad), "años y en 20 años tendre:", type(Total_edad))
