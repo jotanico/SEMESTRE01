10e3b0356aafbdbfd2797caeb8fc56f2499c27ba
jotanico <129216765+jotanico@users.noreply.github.com>
2023-06-27 01:38:51 -0400

    Add files via upload

---
+++ Python/Retos/reto2.py
@@ -0,0 +1,11 @@
+dic = {}
+Nombre_Estudiante= ("ingrese nombre de la asignatura:") 
+Nombre_Asignatura = ("ingrese nombre de la asignatura:") 
+Nota1 = input(float("ingrese Nota 1:"))
+Nota2 = input(float("ingrese Nota 1:"))
+
+Nota_Final = (Nota1 * 0.30) + (Nota2 * 0.70)
+
+print(dic)
+
+
